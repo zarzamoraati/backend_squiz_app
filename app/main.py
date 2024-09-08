48c3f3f0b94226b3b42b9446afeeabc90d05f412
zarzamoraati <zarzamoraati@gmail.com>
2024-09-08 10:05:07 -0600

    v1 pre-liminar proyect working

--- app/main.py
+++ app/main.py
@@ -17,21 +17,19 @@ def home():
 # TODO - Endpoints
 @app.post("/squiz")
 def generate_squiz(query: str = Form(...), img_ctx: Union[UploadFile, List[UploadFile]] = File(...)):
+    content=""
     try:
         model=GenerateQuiz()
         if len(img_ctx) == 1:
             print(img_ctx)
             obj_file=Image.open(img_ctx[0].file)
             content=pytesseract.image_to_string(obj_file)
-            response=model.generate_respose(query=query,img_to_txt=str(content))
-            return response
         else:
-            images=""
             for image in img_ctx:
                 obj_file=Image.open(image.file)
-                content=pytesseract.image_to_string(obj_file)
-                images+=str(content)
-            response=model.generate_respose(query=query,img_to_txt=images)
+                content+=pytesseract.image_to_string(obj_file)
+            response=model.generate_respose(query=query,img_to_txt=content)
+            return response
 
     except Exception as e:
         return {"error":e}
